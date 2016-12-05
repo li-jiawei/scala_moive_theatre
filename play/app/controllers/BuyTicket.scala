846524e84af535334dd1b1d7c8eeddce1daf55ec
lingqiaomin <ling.qi@husky.neu.edu>
2016-12-04 19:06:37 -0500

    ticket2

--- play/app/controllers/BuyTicket.scala
+++ play/app/controllers/BuyTicket.scala
@@ -15,7 +15,7 @@ class BuyTicket extends Controller {
   def buyticket(movieid: String,moviename:String) = Action {
     val orderid = postgres_jdbc.main(movieid, moviename)
     orderid match {
-      case 0 => Ok(views.html.ticketResultFail("failed"))
+      case "0" => Ok(views.html.ticketResultFail("failed"))
       case _ => Ok(views.html.ticketResult(moviename, orderid.toString))
     }
   }
@@ -24,17 +24,25 @@ class BuyTicket extends Controller {
   object postgres_jdbc {
     val conn_str = "jdbc:postgresql://localhost:5432/postgres"
 
-    def main(movieid: String,moviename:String): Int = {
-      var orderid = (Calendar.getInstance().getTimeInMillis%100000000).toInt
+    def main(movieid: String,moviename:String): String = {
+      var time = (Calendar.getInstance().getTimeInMillis%100000000).toInt
+      val orderid = movieid.toString+time.toString
       val conn = DriverManager.getConnection(conn_str, "postgres", "0403")
       try {
 
         val statement = conn.createStatement()
-        val queryShowtime = "SELECT showtime FROM timelist where movieid="+movieid+";"
-        val showtime = statement.executeQuery(queryShowtime)
-        showtime.next()
+//        val queryShowtime = "SELECT showtime FROM timelist where movieid="+movieid+";"
+//        val showtime = statement.executeQuery(queryShowtime)
+//        showtime.next()
+//        val showT = showtime.getString(1)
 
-        val queryOrder = "INSERT INTO orderlist VALUES( "+movieid+", 'User', '"+ moviename+"','"+ showtime.getString(1)+ "', "+orderid+");"
+
+//        val showT="2016-01-01 12:00:00"
+//        val queryOrder = "INSERT INTO orderlist VALUES( "+movieid+", 'User', '"+ moviename+"','"+showT + "', "+orderid+");"
+//        val queryUpdate =  "UPDATE timelist SET remainticket =remainticket-1 WHERE movieid ="+movieid+";"
+
+
+        val queryOrder = "INSERT INTO orderlist1 VALUES( '"+orderid+"');"
         val queryUpdate =  "UPDATE timelist SET remainticket =remainticket-1 WHERE movieid ="+movieid+";"
 
         statement.executeUpdate(queryUpdate)
@@ -45,7 +53,7 @@ class BuyTicket extends Controller {
       } catch{
         case ex: org.postgresql.util.PSQLException => {
           System.out.print(ex)
-          return 0
+          return "0"
         }
       } finally{
         conn.close
--- play/app/controllers/LookUpOrder.scala
+++ play/app/controllers/LookUpOrder.scala
@@ -12,17 +12,24 @@ class LookUpOrder extends Controller {
 
 
   def lookuporder(orderid: String) = Action {
-    val moviename = ""
-    val showtime = ""
-
     val conn_str = "jdbc:postgresql://localhost:5432/postgres"
     val conn = DriverManager.getConnection(conn_str, "postgres", "0403")
     try {
+
       val statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
-      val queryS = "SELECT * FROM orderlist WHERE orderid =" + orderid + " ; "
-      val order = statement.executeQuery(queryS)
+//      val queryS = "SELECT * FROM orderlist WHERE orderid =" + orderid + " ; "
+//      val order = statement.executeQuery(queryS)
+//      order.next
+//      val moviename = order.getString(3).replace("_"," ")
+//      val queryShowtime = "SELECT showtime FROM timelist where movieid="+order.getString(1)+";"
+//              val showtime = statement.executeQuery(queryShowtime)
+//              showtime.next()
+//              val showT = showtime.getString(1)
+      val movieid = orderid.substring(0,10)
+      val query = "SELECT * FROM timelist where movieid="+movieid+";"
+      val order = statement.executeQuery(query)
       order.next
-      Ok (views.html.lookUpOrder(order.getString(3).replace("_"," "), order.getString(4)))
+      Ok (views.html.lookUpOrder(order.getString(1).replace("_"," "), order.getString(4)))
 
     } finally {
       conn.close
